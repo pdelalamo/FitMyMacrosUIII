e9c7e230162b84bfd6b12be2a802f47c52ac3b5b
Pablo <pabloskixd23@gmail.com>
2024-04-11 22:36:41 +0200

    styles

--- styles.ts
+++ styles.ts
@@ -6,21 +6,82 @@ export const styles = StyleSheet.create({
     },
     imageBackground: {
         flex: 1,
-        justifyContent: 'center', // Aligns the children to the center
+        justifyContent: 'center',
     },
     contentContainer: {
         flex: 1,
-        justifyContent: 'space-between', // Distributes children evenly, aligning the first child to the start and the last child to the end
-        padding: 20, // Adds padding inside the container
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+    },
+    contentContainerFeatures: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 0,
+    },
+    containerFeaturesText: {
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        paddingTop: 0,
+        width: '70%',
+        marginTop: '-15%'
+    },
+    contentContainerBottom: {
+        position: 'absolute',
+        bottom: '8%',
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
     },
     middleImage: {
-        width: '100%', // Makes the image full width
-        height: 200, // Fixed height for the image
-        resizeMode: 'contain', // Ensures the image is fully visible
-        alignSelf: 'center', // Centers the image
+        width: '100%',
+        height: '50%',
+        resizeMode: 'contain',
+        alignSelf: 'center',
+    },
+    middleImageFeatures: {
+        flex: 0.8,
+        width: '100%',
+        resizeMode: 'contain',
+        alignSelf: 'center',
+        marginTop: '35%',
+        marginBottom: 0,
+        padding: 0
     },
     buttonContainer: {
-        flexDirection: 'row', // Aligns buttons in a horizontal row
-        justifyContent: 'space-around', // Distributes buttons evenly with space around them
+        flexDirection: 'column',
+        justifyContent: 'center',
+    },
+    buttonContainerFeatures: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+        marginTop: '10%'
+    },
+    buttonGreen: {
+        backgroundColor: 'green',
+        padding: '3%',
+        borderRadius: 10,
+        margin: '3%',
+        width: 300,
+        alignSelf: 'center'
+    },
+    buttonGrey: {
+        backgroundColor: 'grey',
+        padding: '3%',
+        borderRadius: 10,
+        margin: '3%',
+        width: 300,
+        alignSelf: 'center'
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center',
+        fontSize: 20
+    },
+    featureText: {
+        textAlign: 'center',
+        fontSize: 18
     },
 });
