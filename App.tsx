6409556e5b14fb59af71181815e4ea80213e67a5
Pablo <pabloskixd23@gmail.com>
2024-04-13 00:24:56 +0200

    toast messages

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
 import Navigation from './navigation/Navigation';
+import { RootSiblingParent } from 'react-native-root-siblings';
+
 
 function App() {
-  return <Navigation />;
+  return (<RootSiblingParent>
+    <Navigation />
+  </RootSiblingParent>);
 }
 
 export default App;
--- screens/initialQuestions/ChooseDiet.tsx
+++ screens/initialQuestions/ChooseDiet.tsx
@@ -1,22 +1,41 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { View, Text, TouchableOpacity, Button, ImageBackground } from 'react-native';
 import { globalStyles } from '../../globalStyles';
 import { initialQuestionsStyles } from './initialQuestionsStyles';
 import { I18nextProvider } from 'react-i18next';
 import i18n from '../../i18n';
 import { t } from 'i18next';
+import { UserPreferencesProvider, useUserPreferences } from '../../context/UserPreferencesContext';
+import Toast from 'react-native-root-toast';
+
 const dietOptions = ['Omnivore', 'Carnivore', 'Vegan', 'Keto', 'Vegetarian', 'Paleo', 'PescoPollo'];
+
 
 interface Props {
     navigation: any;
 }
 
 const ChooseDiet: React.FC<Props> = ({ navigation }) => {
+    //usage of the context. Basically allows to update the diet type in the context when calling the setDietType function
+    const { setDietType } = useUserPreferences();
+    //initializes selectedDiet to null, and then updates it with each setSelectedDiet call 
     const [selectedDiet, setSelectedDiet] = useState<string | null>(null);
 
     const handleSelectDiet = (diet: string) => {
         setSelectedDiet(diet);
+        setDietType(diet);
     };
+
+    const handleContinue = () => {
+        if (!selectedDiet) {
+            let toast = Toast.show('Please select a diet before continuing', {
+                duration: Toast.durations.LONG,
+            });
+            return;
+        }
+        navigation.navigate('NextScreen');
+    };
+
 
     return (
         <I18nextProvider i18n={i18n}>
@@ -33,7 +52,7 @@ const ChooseDiet: React.FC<Props> = ({ navigation }) => {
                         </TouchableOpacity>
                     ))}
                     <View style={globalStyles.buttonContainerFeatures}>
-                        <TouchableOpacity style={globalStyles.buttonGreen} onPress={() => navigation.navigate('ChooseDiet')}>
+                        <TouchableOpacity style={globalStyles.buttonGreen} onPress={handleContinue}>
                             <Text style={globalStyles.buttonText}>{t('continue')}</Text>
                         </TouchableOpacity>
                     </View>
