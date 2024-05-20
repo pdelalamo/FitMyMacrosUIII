e40bf27602255ce76a725f0432c11c5a3697e35b
Pablo <pabloskixd23@gmail.com>
2024-05-21 00:08:02 +0200

    commented react native iap code

--- App.tsx
+++ App.tsx
@@ -3,32 +3,32 @@ import Navigation from './navigation/Navigation';
 import { RootSiblingParent } from 'react-native-root-siblings';
 import { useEffect } from 'react';
 import { Platform } from 'react-native';
-import {
-  initConnection,
-  endConnection,
-  flushFailedPurchasesCachedAsPendingAndroid,
-} from 'react-native-iap';
+// import {
+//   initConnection,
+//   endConnection,
+//   flushFailedPurchasesCachedAsPendingAndroid,
+// } from 'react-native-iap';
 
 function App() {
-  if (Platform.OS === 'android' || Platform.OS === 'ios') {
-    useEffect(() => {
-      const init = async () => {
-        try {
-          await initConnection();
-          if (Platform.OS === 'android') {
-            flushFailedPurchasesCachedAsPendingAndroid();
-          }
-        }
-        catch (error) {
-          console.error('Error occurred during initilization', error);
-        }
-      }
-      init();
-      return () => {
-        endConnection();
-      }
-    }, [])
-  }
+  // if (Platform.OS === 'android' || Platform.OS === 'ios') {
+  //   useEffect(() => {
+  //     const init = async () => {
+  //       try {
+  //         await initConnection();
+  //         if (Platform.OS === 'android') {
+  //           flushFailedPurchasesCachedAsPendingAndroid();
+  //         }
+  //       }
+  //       catch (error) {
+  //         console.error('Error occurred during initilization', error);
+  //       }
+  //     }
+  //     init();
+  //     return () => {
+  //       endConnection();
+  //     }
+  //   }, [])
+  // }
   return (<RootSiblingParent>
     <Navigation />
   </RootSiblingParent>);
