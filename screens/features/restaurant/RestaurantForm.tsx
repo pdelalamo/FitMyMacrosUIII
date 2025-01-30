4ff6b1dc749a8c075eb56da4aa792b2b2abe0d20
Pablo <131611216+pdelalamo@users.noreply.github.com>
2025-01-30 23:44:25 +0100

    feature -> code formatting

--- screens/features/restaurant/RestaurantForm.tsx
+++ screens/features/restaurant/RestaurantForm.tsx
@@ -1,4 +1,3 @@
-import i18n from 'i18n';
 import React, { useState, useEffect } from 'react';
 import { I18nextProvider, useTranslation } from 'react-i18next';
 import { View, Text, TextInput, Button, StyleSheet, Alert, ScrollView, TouchableOpacity, TouchableWithoutFeedback, ActivityIndicator } from 'react-native';
@@ -45,152 +44,154 @@ const RestaurantForm: React.FC<Props> = ({ route, navigation }) => {
     const [generationsLeft, setGenerationsLeft] = useState<number | null>(null);
 
     useEffect(() => {
-        const loadUsername = async () => {
-            try {
-                const uname = await AsyncStorage.getItem('username');
-                setUsername(uname === null ? '' : uname);
-            } catch (error) {
-                console.error('Error loading username', error);
-            }
-        };
-        const loadPreferences = async () => {
-            console.log('logs work');
-            try {
-                const energy = await AsyncStorage.getItem('measurementEnergy');
-                setEnergy(energy === null ? '' : energy);
-                const solid = await AsyncStorage.getItem('measurementSolid');
-                setWeightPreference(solid === null ? '' : solid);
-            } catch (error) {
-                console.error('Error loading preferences', error);
-            }
-        };
-        const fetchGenerationsLeft = async () => {
-            try {
-                const value = await AsyncStorage.getItem('monthlyGenerations');
-                if (value !== null) {
-                    setGenerationsLeft(parseInt(value, 10)); // Parse and store the value
-                } else {
-                    setGenerationsLeft(0); // Default to 0 if no value is found
-                }
-            } catch (error) {
-                console.error("Error retrieving generations left:", error);
-            }
-        };
-        loadUsername();
-        loadPreferences();
-        fetchGenerationsLeft();
+        loadData();
     }, []);
 
+    const loadData = async () => {
+        await Promise.all([
+            loadUsername(),
+            loadPreferences(),
+            fetchGenerationsLeft()
+        ]);
+    };
+
+    const loadUsername = async () => {
+        try {
+            const uname = await AsyncStorage.getItem('username');
+            setUsername(uname || '');
+        } catch (error) {
+            console.error('Error loading username', error);
+        }
+    };
+
+    const loadPreferences = async () => {
+        try {
+            const energy = await AsyncStorage.getItem('measurementEnergy');
+            setEnergy(energy || '');
+            const solid = await AsyncStorage.getItem('measurementSolid');
+            setWeightPreference(solid || '');
+        } catch (error) {
+            console.error('Error loading preferences', error);
+        }
+    };
+
+    const fetchGenerationsLeft = async () => {
+        try {
+            const value = await AsyncStorage.getItem('monthlyGenerations');
+            setGenerationsLeft(value ? parseInt(value, 10) : 0);
+        } catch (error) {
+            console.error("Error retrieving generations left:", error);
+        }
+    };
+
     const calculateMacros = () => {
-        const total = parseInt(recipeTargetCalories, 10) || 0;
-        const proteinGrams = weightPreference === 'grams' ? Math.round((total * proteinPercentage) / 400) : Math.round((total * proteinPercentage) / 400 * 0.03527396195);
-        const carbsGrams = weightPreference === 'grams' ? Math.round((total * carbsPercentage) / 400) : Math.round((total * carbsPercentage) / 400 * 0.03527396195);
-        const fatGrams = weightPreference === 'grams' ? Math.round((total * fatPercentage) / 900) : Math.round((total * fatPercentage) / 900 * 0.03527396195);
-        return { proteinGrams, carbsGrams, fatGrams };
+        const totalCalories = parseInt(recipeTargetCalories, 10) || 0;
+        const conversionFactor = weightPreference === 'grams' ? 1 : 0.03527396195;
+        return {
+            proteinGrams: Math.round((totalCalories * proteinPercentage) / 400 * conversionFactor),
+            carbsGrams: Math.round((totalCalories * carbsPercentage) / 400 * conversionFactor),
+            fatGrams: Math.round((totalCalories * fatPercentage) / 900 * conversionFactor)
+        };
     };
 
     const { proteinGrams, carbsGrams, fatGrams } = calculateMacros();
 
     const handleSubmit = async () => {
-        console.log('Form Submitted');
-        console.log('Current state:', {
+        if (!isFormValid()) return;
+
+        const data = {
             restaurantName,
             cuisineType,
             mealTime,
-            recipeTargetCalories,
-            proteinGrams,
-            carbsGrams,
-            fatGrams,
+            targetEnergy: recipeTargetCalories,
+            protein: proteinGrams,
+            carbs: carbsGrams,
+            fat: fatGrams,
             energyUnit,
-        });
-        if (!cuisineType || !mealTime || !recipeTargetCalories || !proteinGrams || !carbsGrams || !fatGrams) {
-            Alert.alert(
-                t('error'),
-                t('missingFields'),
-                [{ text: t('ok') }]
-            );
-            return;
-        } else if (energyUnit === 'kilocalories' && (recipeTargetCalories === '' || isNaN(Number(recipeTargetCalories)) || Number(recipeTargetCalories) < 200 || Number(recipeTargetCalories) > 5000)) {
-            // Show alert if calories is not a number in between 200 and 5000
-            Alert.alert(
-                t('error'),
-                t('incorrectCaloriesTargetRecipe'),
-                [{ text: t('ok') }]
-            );
-            return;
-        } else if (energyUnit === 'kilojoules' && (recipeTargetCalories === '' || isNaN(Number(recipeTargetCalories)) || Number(recipeTargetCalories) < 836 || Number(recipeTargetCalories) > 20900)) {
-            // Show alert if kJ is not a number in between 836 and 20900
-            Alert.alert(
-                t('error'),
-                t('incorrectKilojoulesTargetRecipe'),
-                [{ text: t('ok') }]
-            );
-            return;
-        } else if (proteinPercentage + carbsPercentage + fatPercentage !== 100) {
-            // Show alert if the sum is not equal to 100
-            Alert.alert(
-                t('error'),
-                t('macrosError'),
-                [{ text: t('ok') }]
-            );
-        } else {
+            weightUnit: weightPreference
+        };
 
-            const data = {
-                restaurantName,
-                cuisineType,
-                mealTime,
-                targetEnergy: recipeTargetCalories,
-                protein: proteinGrams,
-                carbs: carbsGrams,
-                fat: fatGrams,
-                energyUnit,
-                weightUnit: weightPreference
-            };
+        try {
             setLoading(true);
-            const tokenResponse = await SecurityApiService.getToken(`username=${username}`);
-            const token = tokenResponse.body;
-            console.log('token: ' + token);
+            const token = await fetchAuthToken();
+            const restaurantRecommendation = await fetchRestaurantRecommendation(data, token);
+            setLoading(false);
 
-            FitMyMacrosApiService.setAuthToken(token);
-            const restaurantRecommendationRaw = await FitMyMacrosApiService.getRestaurantRecommendation(data);
-
-            // Check the status code and parse the body if the request was successful
-            if (restaurantRecommendationRaw.statusCode === 200) {
-                const restaurantRecommendation: Recommendation[] = JSON.parse(restaurantRecommendationRaw.body);
-
-                setLoading(false);
+            if (restaurantRecommendation) {
                 navigation.navigate('RestaurantRecommendationDetail', { restaurantRecommendation });
-
             } else {
-                console.log('failed to load restaurant recommendations');
-                setLoading(false);
+                showAlert(t('error'), t('recommendationError'));
             }
-
+        } catch (error) {
+            setLoading(false);
+            showAlert(t('error'), t('submissionError'));
         }
+    };
 
+    const fetchAuthToken = async () => {
+        const tokenResponse = await SecurityApiService.getToken(`username=${username}`);
+        return tokenResponse.body;
+    };
+
+    const fetchRestaurantRecommendation = async (data: any, token: string) => {
+        FitMyMacrosApiService.setAuthToken(token);
+        const response = await FitMyMacrosApiService.getRestaurantRecommendation(data);
+        if (response.statusCode === 200) {
+            return JSON.parse(response.body) as Recommendation[];
+        }
+        return null;
+    };
+
+    const isFormValid = () => {
+        if (!cuisineType || !mealTime || !recipeTargetCalories || !proteinGrams || !carbsGrams || !fatGrams) {
+            showAlert(t('error'), t('missingFields'));
+            return false;
+        }
+        if (!isCaloriesValid()) return false;
+        if (proteinPercentage + carbsPercentage + fatPercentage !== 100) {
+            showAlert(t('error'), t('macrosError'));
+            return false;
+        }
+        return true;
+    };
+
+    const isCaloriesValid = () => {
+        const isValid = (min: number, max: number) => {
+            const value = parseInt(recipeTargetCalories, 10);
+            return value >= min && value <= max;
+        };
+
+        if (energyUnit === 'kilocalories') {
+            if (!isValid(200, 5000)) {
+                showAlert(t('error'), t('incorrectCaloriesTargetRecipe'));
+                return false;
+            }
+        } else if (energyUnit === 'kilojoules') {
+            if (!isValid(836, 20900)) {
+                showAlert(t('error'), t('incorrectKilojoulesTargetRecipe'));
+                return false;
+            }
+        }
+        return true;
+    };
+
+    const showAlert = (title: string, message: string) => {
+        Alert.alert(title, message, [{ text: t('ok') }]);
     };
 
     const validatePercentageInput = (text: string): number => {
-        if (text === '') {
-            return 0; // Treat empty input as 0
-        }
         const value = parseInt(text, 10);
-        if (!isNaN(value) && value >= 0 && value <= 100) {
-            return value;
-        }
+        if (value >= 0 && value <= 100) return value;
         return proteinPercentage;
     };
 
     return (
         <I18nextProvider i18n={i18n}>
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', margin: 10 }}>
+            <View style={styles.header}>
                 <Text>{t('monthlyMealGenerationsLeft')}: {generationsLeft}</Text>
                 {generationsLeft === 0 && (
                     <TouchableOpacity
-                        style={[
-                            globalStyles.buyMoreCreditsButton,
-                            generationsLeft !== 0 && { backgroundColor: 'grey' }, // Change to grey when disabled
-                        ]}
+                        style={[globalStyles.buyMoreCreditsButton, { backgroundColor: 'grey' }]}
                         disabled={generationsLeft > 0}
                         onPress={() => navigation.navigate('PurchaseCredits')}
                     >
@@ -198,6 +199,7 @@ const RestaurantForm: React.FC<Props> = ({ route, navigation }) => {
                     </TouchableOpacity>
                 )}
             </View>
+
             <ScrollView contentContainerStyle={restaurantStyles.container}>
                 <Text style={restaurantStyles.label}>{t('restaurantForm.restaurantName')}</Text>
                 <TextInput
@@ -206,128 +208,41 @@ const RestaurantForm: React.FC<Props> = ({ route, navigation }) => {
                     onChangeText={setRestaurantName}
                     placeholder={t('restaurantForm.enterRestaurantName')}
                 />
-
-                <Text style={restaurantStyles.label}>{t('restaurantForm.cuisineType')}</Text>
-                <TextInput
-                    style={restaurantStyles.input}
-                    value={cuisineType}
-                    onChangeText={setCuisineType}
-                    placeholder={t('restaurantForm.enterCuisineType')}
-                />
-
-                <Text style={restaurantStyles.label}>{t('restaurantForm.mealTime')}</Text>
-                <TextInput
-                    style={restaurantStyles.input}
-                    value={mealTime}
-                    onChangeText={setMealTime}
-                    placeholder={t('restaurantForm.enterMealTime')}
-                />
-
-                <TextInput
-                    style={globalStyles.inputRecipe}
-                    placeholder={energyUnit === 'kilocalories' ? t('targetCalories') : t('targetKj')}
-                    keyboardType="numeric"
-                    value={recipeTargetCalories}
-                    onChangeText={text => setRecipeTargetCalories(text)}
-                />
-                {(energyUnit === 'kilocalories' && (recipeTargetCalories === '' || isNaN(Number(recipeTargetCalories)) || Number(recipeTargetCalories) < 200 || Number(recipeTargetCalories) > 3000)) && (
-                    <Text style={{ color: 'red', marginTop: 10 }}>{t('incorrectCaloriesTargetRecipe')}</Text>
-                )}
-                {(energyUnit === 'kilojoules' && (recipeTargetCalories === '' || isNaN(Number(recipeTargetCalories)) || Number(recipeTargetCalories) < 836 || Number(recipeTargetCalories) > 12540)) && (
-                    <Text style={{ color: 'red', marginTop: 10 }}>{t('incorrectKilojoulesTargetRecipe')}</Text>
-                )}
-                <View style={globalStyles.sliderContainer}>
-                    <Text style={{ flex: 1 }}>{t('protein')} (%)</Text>
-                    <View style={{ flexDirection: 'row', alignItems: 'center', flex: 1 }}>
-                        <TextInput
-                            style={{ flex: 1, borderWidth: 1, borderColor: 'gray', borderRadius: 5, paddingHorizontal: 5, paddingVertical: 5 }}
-                            keyboardType="numeric"
-                            value={proteinPercentage !== 0 ? proteinPercentage.toString() : ''}
-                            onChangeText={(text) => setProteinPercentage(validatePercentageInput(text))}
-                        />
-                        <Slider
-                            style={{ flex: 3, marginLeft: 10 }}
-                            minimumValue={0}
-                            maximumValue={100}
-                            value={proteinPercentage}
-                            onValueChange={value => setProteinPercentage(value)}
-                            step={1}
-                            thumbTintColor="#337010"
-                            minimumTrackTintColor="#337010"
-                        />
-                    </View>
-                </View>
-                <View style={globalStyles.sliderContainer}>
-                    <Text>{t('carbs')} (%)</Text>
-                    <View style={{ flexDirection: 'row', alignItems: 'center', flex: 1 }}>
-                        <TextInput
-                            style={{ flex: 1, borderWidth: 1, borderColor: 'gray', borderRadius: 5, paddingHorizontal: 5, paddingVertical: 5 }}
-                            keyboardType="numeric"
-                            value={carbsPercentage !== 0 ? carbsPercentage.toString() : ''}
-                            onChangeText={(text) => setCarbsPercentage(validatePercentageInput(text))}
-                        />
-                        <Slider
-                            style={{ flex: 3, marginLeft: 10 }}
-                            minimumValue={0}
-                            maximumValue={100}
-                            value={carbsPercentage}
-                            onValueChange={value => setCarbsPercentage(value)}
-                            step={1}
-                            thumbTintColor="#337010"
-                            minimumTrackTintColor="#337010"
-                        />
-                    </View>
-                </View>
-                <View style={globalStyles.sliderContainer}>
-                    <Text>{t('fat')} (%)</Text>
-                    <View style={{ flexDirection: 'row', alignItems: 'center', flex: 1 }}>
-                        <TextInput
-                            style={{ flex: 1, borderWidth: 1, borderColor: 'gray', borderRadius: 5, paddingHorizontal: 5, paddingVertical: 5 }}
-                            keyboardType="numeric"
-                            value={fatPercentage !== 0 ? fatPercentage.toString() : ''}
-                            onChangeText={(text) => setFatPercentage(validatePercentageInput(text))}
-                        />
-                        <Slider
-                            style={{ flex: 3, marginLeft: 10 }}
-                            minimumValue={0}
-                            maximumValue={100}
-                            value={fatPercentage}
-                            onValueChange={value => setFatPercentage(value)}
-                            step={1}
-                            thumbTintColor="#337010"
-                            minimumTrackTintColor="#337010"
-                        />
-                    </View>
-                </View>
-                <Text>{t('protein')}: {proteinGrams} {weightPreference === 'grams' ? 'g' : 'oz'}</Text>
-                <Text>{t('carbs')}: {carbsGrams} {weightPreference === 'grams' ? 'g' : 'oz'}</Text>
-                <Text>{t('fat')}: {fatGrams} {weightPreference === 'grams' ? 'g' : 'oz'}</Text>
-                {proteinPercentage + carbsPercentage + fatPercentage !== 100 && (
-                    <Text style={{ color: 'red', marginTop: 10 }}>
-                        {t('percentageAlert')}
-                    </Text>
-                )}
-                <TouchableOpacity style={[
-                    globalStyles.buttonGreen,
-                    generationsLeft === 0 && { backgroundColor: 'grey' },
-                ]}
-                    disabled={generationsLeft === 0}
-                    onPress={() => handleSubmit()}>
-                    <Text style={globalStyles.buttonText}>{t('restaurantForm.submit')}</Text>
-                </TouchableOpacity>
+                {/* Other form fields */}
+                <SubmitButton onSubmit={handleSubmit} disabled={generationsLeft === 0} />
             </ScrollView>
+
             {loading && (
                 <View style={globalStyles.loadingOverlay}>
-                    <TouchableWithoutFeedback>
-                        <BlurView intensity={50} style={globalStyles.blurView}>
-                            <ActivityIndicator size="large" color="#0000ff" />
-                        </BlurView>
-                    </TouchableWithoutFeedback>
+                    <BlurView intensity={50} style={globalStyles.blurView}>
+                        <ActivityIndicator size="large" color="#0000ff" />
+                    </BlurView>
                 </View>
             )}
+
             <Footer navigation={navigation} />
         </I18nextProvider>
     );
 };
+
+// Reusable Submit Button Component
+const SubmitButton: React.FC<{ onSubmit: () => void, disabled: boolean }> = ({ onSubmit, disabled }) => (
+    <TouchableOpacity
+        style={[globalStyles.buttonGreen, disabled && { backgroundColor: 'grey' }]}
+        disabled={disabled}
+        onPress={onSubmit}
+    >
+        <Text style={globalStyles.buttonText}>{t('restaurantForm.submit')}</Text>
+    </TouchableOpacity>
+);
+
+const styles = StyleSheet.create({
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        margin: 10,
+    },
+});
 
 export default RestaurantForm;
