9056a00353f31d40df0c0bad896b4df99734b67e
Pablo <131611216+pdelalamo@users.noreply.github.com>
2025-01-31 00:14:39 +0100

    feature -> code formatting

--- screens/initialQuestions/TargetCaloriesAndMacros.tsx
+++ screens/initialQuestions/TargetCaloriesAndMacros.tsx
@@ -1,10 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity, ImageBackground, ScrollView, Alert, TextInput, TouchableWithoutFeedback, ActivityIndicator } from 'react-native';
+import React, { useEffect, useState, useCallback } from 'react';
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    ImageBackground,
+    ScrollView,
+    Alert,
+    TextInput,
+    TouchableWithoutFeedback,
+    ActivityIndicator,
+} from 'react-native';
 import { globalStyles } from '../../globalStyles';
 import { initialQuestionsStyles } from './initialQuestionsStyles';
 import { I18nextProvider, useTranslation } from 'react-i18next';
 import i18n from '../../i18n';
-import { t } from 'i18next';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Slider from '@react-native-community/slider';
 import { BlurView } from 'expo-blur';
@@ -15,7 +24,6 @@ interface Props {
 
 const TargetCaloriesAndMacros: React.FC<Props> = ({ navigation }) => {
     const { t } = useTranslation();
-
     const [energyUnit, setEnergyUnit] = useState('');
     const [weightUnit, setWeightUnit] = useState('');
     const [targetCalories, setTargetCalories] = useState('');
@@ -25,47 +33,56 @@ const TargetCaloriesAndMacros: React.FC<Props> = ({ navigation }) => {
     const [loading, setLoading] = useState(false);
     const [username, setUsername] = useState('');
 
-    useEffect(() => {
-        const loadUserData = async () => {
+    const loadUserData = useCallback(async () => {
+        try {
             const uname = await AsyncStorage.getItem('username');
             const energy = await AsyncStorage.getItem('measurementEnergy');
             const weight = await AsyncStorage.getItem('measurementSolid');
             setUsername(uname || '');
             setEnergyUnit(energy || '');
             setWeightUnit(weight || '');
-        };
-        loadUserData();
+        } catch (error) {
+            console.error('Error loading user data', error);
+        }
     }, []);
 
-    const calculateMacros = () => {
-        const total = parseInt(targetCalories, 10) || 0;
-        const proteinGrams = weightUnit === 'grams' ? Math.round((total * proteinPercentage) / 400) : Math.round((total * proteinPercentage) / 400 * 0.03527396195);
-        const carbsGrams = weightUnit === 'grams' ? Math.round((total * carbsPercentage) / 400) : Math.round((total * carbsPercentage) / 400 * 0.03527396195);
-        const fatGrams = weightUnit === 'grams' ? Math.round((total * fatPercentage) / 900) : Math.round((total * fatPercentage) / 900 * 0.03527396195);
+    useEffect(() => {
+        loadUserData();
+    }, [loadUserData]);
+
+    const calculateMacros = useCallback(() => {
+        const totalCalories = parseInt(targetCalories, 10) || 0;
+        const conversionFactor = weightUnit === 'grams' ? 1 : 0.03527396195;
+        const proteinGrams = Math.round((totalCalories * proteinPercentage) / 400 * conversionFactor);
+        const carbsGrams = Math.round((totalCalories * carbsPercentage) / 400 * conversionFactor);
+        const fatGrams = Math.round((totalCalories * fatPercentage) / 900 * conversionFactor);
         return { proteinGrams, carbsGrams, fatGrams };
-    };
+    }, [targetCalories, proteinPercentage, carbsPercentage, fatPercentage, weightUnit]);
 
     const { proteinGrams, carbsGrams, fatGrams } = calculateMacros();
 
-    const validatePercentageInput = (text: string) => {
-        if (text === '') {
-            return 0; // Treat empty input as 0
-        }
+    const validatePercentageInput = (text: string, defaultValue: number) => {
         const value = parseInt(text, 10);
-        return !isNaN(value) && value >= 0 && value <= 100 ? value : proteinPercentage;
+        return Number.isNaN(value) || value < 0 || value > 100 ? defaultValue : value;
+    };
+
+    const validateTargetCalories = (unit: string, calories: string) => {
+        const cal = Number(calories);
+        return unit === 'kilocalories'
+            ? cal >= 1000 && cal <= 10000
+            : cal >= 4184 && cal <= 41840;
     };
 
     const handleSaveSettings = async () => {
-        if (energyUnit === 'kilocalories' && (targetCalories === '' || isNaN(Number(targetCalories)) || Number(targetCalories) < 1000 || Number(targetCalories) > 10000)) {
-            Alert.alert(t('error'), t('incorrectCaloriesTarget'), [{ text: t('ok') }]);
-            return;
-        } else if (energyUnit === 'kilojoules' && (targetCalories === '' || isNaN(Number(targetCalories)) || Number(targetCalories) < 4184 || Number(targetCalories) > 41840)) {
-            Alert.alert(t('error'), t('incorrectKilojoulesTarget'), [{ text: t('ok') }]);
+        const isValidCalories = validateTargetCalories(energyUnit, targetCalories);
+
+        if (!isValidCalories) {
+            const errorMessage = energyUnit === 'kilocalories' ? t('incorrectCaloriesTarget') : t('incorrectKilojoulesTarget');
+            Alert.alert(t('error'), errorMessage, [{ text: t('ok') }]);
             return;
         }
 
-        const sum = proteinPercentage + carbsPercentage + fatPercentage;
-        if (sum !== 100) {
+        if (proteinPercentage + carbsPercentage + fatPercentage !== 100) {
             Alert.alert(t('error'), t('macrosError'), [{ text: t('ok') }]);
             return;
         }
@@ -76,99 +93,74 @@ const TargetCaloriesAndMacros: React.FC<Props> = ({ navigation }) => {
             await AsyncStorage.setItem('proteinPercentage', proteinGrams.toString());
             await AsyncStorage.setItem('carbsPercentage', carbsGrams.toString());
             await AsyncStorage.setItem('fatPercentage', fatGrams.toString());
-            setLoading(false);
             navigation.navigate('FreeTrialScreen');
         } catch (error) {
             console.error('Error saving settings', error);
+        } finally {
             setLoading(false);
         }
     };
 
+    const renderMacroSlider = (label: string, percentage: number, setPercentage: Function) => (
+        <View style={globalStyles.sliderContainer}>
+            <Text style={{ flex: 1 }}>{`${label} (%)`}</Text>
+            <View style={{ flexDirection: 'row', alignItems: 'center', flex: 1 }}>
+                <TextInput
+                    style={{ flex: 1, borderWidth: 1, borderColor: 'gray', borderRadius: 5, paddingHorizontal: 5, paddingVertical: 5 }}
+                    keyboardType="numeric"
+                    value={percentage !== 0 ? percentage.toString() : ''}
+                    onChangeText={(text) => setPercentage(validatePercentageInput(text, percentage))}
+                />
+                <Slider
+                    style={{ flex: 3, marginLeft: 10 }}
+                    minimumValue={0}
+                    maximumValue={100}
+                    value={percentage}
+                    onValueChange={(value) => setPercentage(value)}
+                    step={1}
+                    thumbTintColor="#337010"
+                    minimumTrackTintColor="#337010"
+                />
+            </View>
+        </View>
+    );
+
     return (
         <I18nextProvider i18n={i18n}>
-            <ImageBackground source={require('../../assets/images/main_background.png')} resizeMode="cover" style={globalStyles.imageBackground}>
+            <ImageBackground
+                source={require('../../assets/images/main_background.png')}
+                resizeMode="cover"
+                style={globalStyles.imageBackground}
+            >
                 <View style={initialQuestionsStyles.container}>
                     <ScrollView contentContainerStyle={initialQuestionsStyles.scrollViewContent}>
-                        <Text style={initialQuestionsStyles.titleTarget}>{energyUnit === 'kilocalories' ? t('setupTargetCalories') : t('setupTargetKj')}</Text>
+                        <Text style={initialQuestionsStyles.titleTarget}>
+                            {energyUnit === 'kilocalories' ? t('setupTargetCalories') : t('setupTargetKj')}
+                        </Text>
                         <TextInput
                             style={[globalStyles.input]}
                             placeholder={energyUnit === 'kilocalories' ? t('targetCalories') : t('targetKj')}
                             keyboardType="numeric"
                             value={targetCalories}
-                            onChangeText={text => setTargetCalories(text)}
+                            onChangeText={(text) => setTargetCalories(text)}
                         />
-                        {(energyUnit === 'kilocalories' && (targetCalories === '' || isNaN(Number(targetCalories)) || Number(targetCalories) < 1000 || Number(targetCalories) > 10000)) && (
-                            <Text style={{ color: 'red', marginTop: 10 }}>{t('incorrectCaloriesTarget')}</Text>
+                        {!validateTargetCalories(energyUnit, targetCalories) && (
+                            <Text style={{ color: 'red', marginTop: 10 }}>
+                                {energyUnit === 'kilocalories' ? t('incorrectCaloriesTarget') : t('incorrectKilojoulesTarget')}
+                            </Text>
                         )}
-                        {(energyUnit === 'kilojoules' && (targetCalories === '' || isNaN(Number(targetCalories)) || Number(targetCalories) < 4184 || Number(targetCalories) > 41840)) && (
-                            <Text style={{ color: 'red', marginTop: 10 }}>{t('incorrectKilojoulesTarget')}</Text>
-                        )}
-                        <View style={globalStyles.sliderContainer}>
-                            <Text style={{ flex: 1 }}>{t('protein')} (%)</Text>
-                            <View style={{ flexDirection: 'row', alignItems: 'center', flex: 1 }}>
-                                <TextInput
-                                    style={{ flex: 1, borderWidth: 1, borderColor: 'gray', borderRadius: 5, paddingHorizontal: 5, paddingVertical: 5 }}
-                                    keyboardType="numeric"
-                                    value={proteinPercentage !== 0 ? proteinPercentage.toString() : ''}
-                                    onChangeText={(text) => setProteinPercentage(validatePercentageInput(text))}
-                                />
-                                <Slider
-                                    style={{ flex: 3, marginLeft: 10 }}
-                                    minimumValue={0}
-                                    maximumValue={100}
-                                    value={proteinPercentage}
-                                    onValueChange={value => setProteinPercentage(value)}
-                                    step={1}
-                                    thumbTintColor="#337010"
-                                    minimumTrackTintColor="#337010"
-                                />
-                            </View>
-                        </View>
-                        <View style={globalStyles.sliderContainer}>
-                            <Text>{t('carbs')} (%)</Text>
-                            <View style={{ flexDirection: 'row', alignItems: 'center', flex: 1 }}>
-                                <TextInput
-                                    style={{ flex: 1, borderWidth: 1, borderColor: 'gray', borderRadius: 5, paddingHorizontal: 5, paddingVertical: 5 }}
-                                    keyboardType="numeric"
-                                    value={carbsPercentage !== 0 ? carbsPercentage.toString() : ''}
-                                    onChangeText={(text) => setCarbsPercentage(validatePercentageInput(text))}
-                                />
-                                <Slider
-                                    style={{ flex: 3, marginLeft: 10 }}
-                                    minimumValue={0}
-                                    maximumValue={100}
-                                    value={carbsPercentage}
-                                    onValueChange={value => setCarbsPercentage(value)}
-                                    step={1}
-                                    thumbTintColor="#337010"
-                                    minimumTrackTintColor="#337010"
-                                />
-                            </View>
-                        </View>
-                        <View style={globalStyles.sliderContainer}>
-                            <Text>{t('fat')} (%)</Text>
-                            <View style={{ flexDirection: 'row', alignItems: 'center', flex: 1 }}>
-                                <TextInput
-                                    style={{ flex: 1, borderWidth: 1, borderColor: 'gray', borderRadius: 5, paddingHorizontal: 5, paddingVertical: 5 }}
-                                    keyboardType="numeric"
-                                    value={fatPercentage !== 0 ? fatPercentage.toString() : ''}
-                                    onChangeText={(text) => setFatPercentage(validatePercentageInput(text))}
-                                />
-                                <Slider
-                                    style={{ flex: 3, marginLeft: 10 }}
-                                    minimumValue={0}
-                                    maximumValue={100}
-                                    value={fatPercentage}
-                                    onValueChange={value => setFatPercentage(value)}
-                                    step={1}
-                                    thumbTintColor="#337010"
-                                    minimumTrackTintColor="#337010"
-                                />
-                            </View>
-                        </View>
-                        <Text style={globalStyles.macroText}>{t('protein')}: {proteinGrams} {weightUnit === 'grams' ? 'g' : 'oz'}</Text>
-                        <Text style={globalStyles.macroText}>{t('carbs')}: {carbsGrams} {weightUnit === 'grams' ? 'g' : 'oz'}</Text>
-                        <Text style={globalStyles.macroText}>{t('fat')}: {fatGrams} {weightUnit === 'grams' ? 'g' : 'oz'}</Text>
+                        {renderMacroSlider(t('protein'), proteinPercentage, setProteinPercentage)}
+                        {renderMacroSlider(t('carbs'), carbsPercentage, setCarbsPercentage)}
+                        {renderMacroSlider(t('fat'), fatPercentage, setFatPercentage)}
+                        <Text style={globalStyles.macroText}>
+                            {t('protein')}: {proteinGrams} {weightUnit === 'grams' ? 'g' : 'oz'}
+                        </Text>
+                        <Text style={globalStyles.macroText}>
+                            {t('carbs')}: {carbsGrams} {weightUnit === 'grams' ? 'g' : 'oz'}
+                        </Text>
+                        <Text style={globalStyles.macroText}>
+                            {t('fat')}: {fatGrams} {weightUnit === 'grams' ? 'g' : 'oz'}
+                        </Text>
                         {proteinPercentage + carbsPercentage + fatPercentage !== 100 && (
                             <Text style={{ color: 'red', marginTop: 10 }}>{t('percentageAlert')}</Text>
                         )}
