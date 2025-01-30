fc42dde9029a96529c7442e37be2511a59ad9b2a
Pablo <131611216+pdelalamo@users.noreply.github.com>
2025-01-31 00:22:54 +0100

    feature -> code formatting

--- screens/MainScreen.tsx
+++ screens/MainScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, FC } from 'react';
 import { View, Text, TouchableOpacity, ScrollView, Alert, Modal, StyleSheet, Button } from 'react-native';
 import { I18nextProvider } from 'react-i18next';
 import { t } from 'i18next';
@@ -15,7 +15,7 @@ interface Props {
     navigation: any;
 }
 
-const MainScreen: React.FC<Props> = ({ navigation }) => {
+const MainScreen: FC<Props> = ({ navigation }) => {
     const [meals, setMeals] = useState<Meal[]>([]);
     const [selectedMeal, setSelectedMeal] = useState<Meal | null>(null);
     const [modalVisible, setModalVisible] = useState<boolean>(false);
@@ -26,57 +26,53 @@ const MainScreen: React.FC<Props> = ({ navigation }) => {
     const [targetCarbs, setTargetCarbs] = useState<number>(0);
     const [targetFat, setTargetFat] = useState<number>(0);
 
-    const totalCalories = meals.reduce((total, meal) => total + parseFloat(meal.calories.replace(/[^0-9.-]/g, '')), 0);
-    const proteinConsumed = meals.reduce((total, meal) => total + parseFloat(meal.protein.replace(/[^0-9.-]/g, '')), 0);
-    const carbsConsumed = meals.reduce((total, meal) => total + parseFloat(meal.carbs.replace(/[^0-9.-]/g, '')), 0);
-    const fatConsumed = meals.reduce((total, meal) => total + parseFloat(meal.fat.replace(/[^0-9.-]/g, '')), 0);
     const isFocused = useIsFocused();
 
     useEffect(() => {
-        const loadTargetCalsAndMacros = async () => {
-            try {
-                const storedTargetCalories = await AsyncStorage.getItem('targetCalories');
-                const storedTargetProtein = await AsyncStorage.getItem('proteinPercentage');
-                const storedTargetCarbs = await AsyncStorage.getItem('carbsPercentage');
-                const storedTargetFat = await AsyncStorage.getItem('fatPercentage');
-
-                const targetCalories = storedTargetCalories ? parseFloat(storedTargetCalories) : 0;
-                const targetProtein = storedTargetProtein ? parseFloat(storedTargetProtein) : 0;
-                const targetCarbs = storedTargetCarbs ? parseFloat(storedTargetCarbs) : 0;
-                const targetFat = storedTargetFat ? parseFloat(storedTargetFat) : 0;
-
-                // Ensure that no value is Infinity or NaN
-                setTargetCalories(isFinite(targetCalories) ? targetCalories : 0);
-                setTargetProtein(isFinite(targetProtein) ? targetProtein : 0);
-                setTargetCarbs(isFinite(targetCarbs) ? targetCarbs : 0);
-                setTargetFat(isFinite(targetFat) ? targetFat : 0);
-            } catch (error) {
-                console.error('Error loading calories and macronutrients:', error);
-            }
-        };
-
         loadTargetCalsAndMacros();
     }, []);
 
     useEffect(() => {
-        const loadDailyMeals = async () => {
-            try {
-                const mealsData = await AsyncStorage.getItem('meals');
-                const solid = await AsyncStorage.getItem('measurementSolid');
-                setMeasurement(solid === null ? '' : solid);
-                const energy = await AsyncStorage.getItem('measurementEnergy');
-                setEnergy(energy === null ? '' : energy);
-                if (mealsData) {
-                    const parsedMeals: Meal[] = JSON.parse(mealsData);
-                    setMeals(parsedMeals);
-                }
-            } catch (error) {
-                console.error('Error loading daily meals:', error);
-            }
-        };
-
         loadDailyMeals();
     }, [isFocused]);
+
+    const loadTargetCalsAndMacros = async () => {
+        try {
+            const storedValues = await Promise.all([
+                AsyncStorage.getItem('targetCalories'),
+                AsyncStorage.getItem('proteinPercentage'),
+                AsyncStorage.getItem('carbsPercentage'),
+                AsyncStorage.getItem('fatPercentage'),
+            ]);
+
+            const [calories, protein, carbs, fat] = storedValues.map(value => parseFloat(value || '0'));
+            setTargetCalories(isFinite(calories) ? calories : 0);
+            setTargetProtein(isFinite(protein) ? protein : 0);
+            setTargetCarbs(isFinite(carbs) ? carbs : 0);
+            setTargetFat(isFinite(fat) ? fat : 0);
+        } catch (error) {
+            console.error('Error loading calories and macronutrients:', error);
+        }
+    };
+
+    const loadDailyMeals = async () => {
+        try {
+            const [mealsData, solid, energy] = await Promise.all([
+                AsyncStorage.getItem('meals'),
+                AsyncStorage.getItem('measurementSolid'),
+                AsyncStorage.getItem('measurementEnergy'),
+            ]);
+
+            setMeasurement(solid || '');
+            setEnergy(energy || '');
+            if (mealsData) {
+                const parsedMeals: Meal[] = JSON.parse(mealsData);
+                setMeals(parsedMeals);
+            }
+        } catch (error) {
+            console.error('Error loading daily meals:', error);
+        }
+    };
 
     const deleteMeal = async (mealToDelete: Meal) => {
         const updatedMeals = meals.filter(meal => meal.id !== mealToDelete.id);
@@ -102,124 +98,149 @@ const MainScreen: React.FC<Props> = ({ navigation }) => {
         setModalVisible(false);
     };
 
-    function generateRecipes() {
+    const generateRecipes = () => {
         navigation.navigate('RecipeGeneration');
-    }
+    };
 
-    // Safety check for percentages
-    const calculatePercentage = (consumed: number, target: number) => {
+    const calculatePercentage = (consumed: number, target: number): number => {
         return target > 0 ? (consumed / target) * 100 : 0;
+    };
+
+    const totalCalories = calculateTotal(meals, 'calories');
+    const proteinConsumed = calculateTotal(meals, 'protein');
+    const carbsConsumed = calculateTotal(meals, 'carbs');
+    const fatConsumed = calculateTotal(meals, 'fat');
+
+    const calculateTotal = (items: Meal[], prop: 'calories' | 'protein' | 'carbs' | 'fat'): number => {
+        return items.reduce((total, item) => total + parseFloat(item[prop].replace(/[^0-9.-]/g, '')), 0);
     };
 
     return (
         <I18nextProvider i18n={i18n}>
             <View style={globalStyles.containerMain}>
-                <View style={globalStyles.headerWithBackground}>
-                    <View style={globalStyles.caloriesContainer}>
-                        <CircularProgress
-                            size={100}
-                            strokeWidth={10}
-                            percentage={calculatePercentage(totalCalories, targetCalories)}
-                            color="green"
-                        />
-                        <Text style={globalStyles.caloriesText}>
-                            {totalCalories} {t('of')} {targetCalories} {energyUnit === 'kilocalories' ? 'kcal' : 'kJ'}
-                        </Text>
-                    </View>
-                    <View style={globalStyles.macrosContainer}>
-                        <View style={globalStyles.macroBox}>
-                            <CircularProgress
-                                size={60}
-                                strokeWidth={6}
-                                percentage={calculatePercentage(proteinConsumed, targetProtein)}
-                                color="blue"
-                            />
-                            <Text style={globalStyles.macroText}>{t('protein')}:</Text>
-                            <Text style={globalStyles.macroText}>
-                                {proteinConsumed}/{targetProtein} {measurementUnit}
-                            </Text>
-                        </View>
-                        <View style={globalStyles.macroBox}>
-                            <CircularProgress
-                                size={60}
-                                strokeWidth={6}
-                                percentage={calculatePercentage(carbsConsumed, targetCarbs)}
-                                color="orange"
-                            />
-                            <Text style={globalStyles.macroText}>{t('carbs')}:</Text>
-                            <Text style={globalStyles.macroText}>
-                                {carbsConsumed}/{targetCarbs} {measurementUnit}
-                            </Text>
-                        </View>
-                        <View style={globalStyles.macroBox}>
-                            <CircularProgress
-                                size={60}
-                                strokeWidth={6}
-                                percentage={calculatePercentage(fatConsumed, targetFat)}
-                                color="red"
-                            />
-                            <Text style={globalStyles.macroText}>{t('fat')}:</Text>
-                            <Text style={globalStyles.macroText}>
-                                {fatConsumed}/{targetFat} {measurementUnit}
-                            </Text>
-                        </View>
-                    </View>
-                </View>
-                <ScrollView style={globalStyles.mealsContainer}>
-                    {meals.map(meal => (
-                        <View key={meal.id} style={globalStyles.mealBox2}>
-                            <TouchableOpacity
-                                style={{ flex: 1 }}
-                                onPress={() => {
-                                    const recipeData = {
-                                        name: meal.name,
-                                        cookingTime: meal.cookingTime,
-                                        calories: meal.calories,
-                                        protein: meal.protein,
-                                        carbs: meal.carbs,
-                                        fat: meal.fat,
-                                        ingredients: meal.ingredients,
-                                        cookingProcess: meal.cookingProcess
-                                    };
-                                    console.log('Navigating with recipeData:', recipeData);
-                                    navigation.navigate('OpenRecipeDetail', { recipeData });
-                                }}
-                            >
-                                <Text style={globalStyles.mealName}>{meal.name}</Text>
-                                <Text style={globalStyles.mealCalories}>{meal.calories} {energyUnit}</Text>
-                            </TouchableOpacity>
-                            <TouchableOpacity onPress={() => confirmDeleteMeal(meal)}>
-                                <Ionicons name="trash" size={24} color="red" />
-                            </TouchableOpacity>
-                        </View>
-                    ))}
-                </ScrollView>
-                <TouchableOpacity style={globalStyles.addButton} onPress={() => generateRecipes()}>
-                    <Text style={globalStyles.addButtonText}>{t('addMeal')}</Text>
-                </TouchableOpacity>
+                <Header totalCalories={totalCalories} targetCalories={targetCalories} energyUnit={energyUnit} />
+                <MacroOverview
+                    protein={proteinConsumed}
+                    targetProtein={targetProtein}
+                    carbs={carbsConsumed}
+                    targetCarbs={targetCarbs}
+                    fat={fatConsumed}
+                    targetFat={targetFat}
+                    measurementUnit={measurementUnit}
+                />
+                <MealList meals={meals} navigation={navigation} energyUnit={energyUnit} confirmDeleteMeal={confirmDeleteMeal} />
+                <AddMealButton generateRecipes={generateRecipes} />
                 <Footer navigation={navigation} />
-                <Modal
-                    animationType="slide"
-                    transparent={true}
-                    visible={modalVisible}
-                    onRequestClose={() => {
-                        setModalVisible(!modalVisible);
-                    }}
-                >
-                    <View style={styles.centeredView}>
-                        <View style={styles.modalView}>
-                            <Text style={styles.modalText}>{t('confirmDeleteMeal')}</Text>
-                            <View style={styles.buttonContainer}>
-                                <Button title={t('cancel')} onPress={handleCancel} />
-                                <Button title={t('delete')} onPress={handleDeleteConfirmation} color="red" />
-                            </View>
-                        </View>
-                    </View>
-                </Modal>
+                <ConfirmationModal modalVisible={modalVisible} handleDeleteConfirmation={handleDeleteConfirmation} handleCancel={handleCancel} />
             </View>
         </I18nextProvider>
     );
 };
+
+const Header: FC<{ totalCalories: number; targetCalories: number; energyUnit: string }> = ({ totalCalories, targetCalories, energyUnit }) => (
+    <View style={globalStyles.headerWithBackground}>
+        <View style={globalStyles.caloriesContainer}>
+            <CircularProgress size={100} strokeWidth={10} percentage={calculatePercentage(totalCalories, targetCalories)} color="green" />
+            <Text style={globalStyles.caloriesText}>
+                {totalCalories} {t('of')} {targetCalories} {energyUnit === 'kilocalories' ? 'kcal' : 'kJ'}
+            </Text>
+        </View>
+    </View>
+);
+
+const MacroOverview: FC<{ protein: number; targetProtein: number; carbs: number; targetCarbs: number; fat: number; targetFat: number; measurementUnit: string }> = ({
+    protein,
+    targetProtein,
+    carbs,
+    targetCarbs,
+    fat,
+    targetFat,
+    measurementUnit,
+}) => (
+    <View style={globalStyles.macrosContainer}>
+        <MacroItem type="protein" value={protein} target={targetProtein} measurementUnit={measurementUnit} />
+        <MacroItem type="carbs" value={carbs} target={targetCarbs} measurementUnit={measurementUnit} />
+        <MacroItem type="fat" value={fat} target={targetFat} measurementUnit={measurementUnit} />
+    </View>
+);
+
+const MacroItem: FC<{ type: string; value: number; target: number; measurementUnit: string }> = ({ type, value, target, measurementUnit }) => {
+    const colorMap = { protein: 'blue', carbs: 'orange', fat: 'red' };
+    return (
+        <View style={globalStyles.macroBox}>
+            <CircularProgress size={60} strokeWidth={6} percentage={calculatePercentage(value, target)} color={colorMap[type]} />
+            <Text style={globalStyles.macroText}>{t(type)}:</Text>
+            <Text style={globalStyles.macroText}>
+                {value}/{target} {measurementUnit}
+            </Text>
+        </View>
+    );
+};
+
+const MealList: FC<{ meals: Meal[]; navigation: any; energyUnit: string; confirmDeleteMeal: (meal: Meal) => void }> = ({ meals, navigation, energyUnit, confirmDeleteMeal }) => (
+    <ScrollView style={globalStyles.mealsContainer}>
+        {meals.map(meal => (
+            <View key={meal.id} style={globalStyles.mealBox2}>
+                <TouchableOpacity
+                    style={{ flex: 1 }}
+                    onPress={() => {
+                        const recipeData = {
+                            name: meal.name,
+                            cookingTime: meal.cookingTime,
+                            calories: meal.calories,
+                            protein: meal.protein,
+                            carbs: meal.carbs,
+                            fat: meal.fat,
+                            ingredients: meal.ingredients,
+                            cookingProcess: meal.cookingProcess,
+                        };
+                        console.log('Navigating with recipeData:', recipeData);
+                        navigation.navigate('OpenRecipeDetail', { recipeData });
+                    }}
+                >
+                    <Text style={globalStyles.mealName}>{meal.name}</Text>
+                    <Text style={globalStyles.mealCalories}>
+                        {meal.calories} {energyUnit}
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => confirmDeleteMeal(meal)}>
+                    <Ionicons name="trash" size={24} color="red" />
+                </TouchableOpacity>
+            </View>
+        ))}
+    </ScrollView>
+);
+
+const AddMealButton: FC<{ generateRecipes: () => void }> = ({ generateRecipes }) => (
+    <TouchableOpacity style={globalStyles.addButton} onPress={generateRecipes}>
+        <Text style={globalStyles.addButtonText}>{t('addMeal')}</Text>
+    </TouchableOpacity>
+);
+
+const ConfirmationModal: FC<{ modalVisible: boolean; handleDeleteConfirmation: () => void; handleCancel: () => void }> = ({
+    modalVisible,
+    handleDeleteConfirmation,
+    handleCancel,
+}) => (
+    <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+            handleCancel();
+        }}
+    >
+        <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+                <Text style={styles.modalText}>{t('confirmDeleteMeal')}</Text>
+                <View style={styles.buttonContainer}>
+                    <Button title={t('cancel')} onPress={handleCancel} />
+                    <Button title={t('delete')} onPress={handleDeleteConfirmation} color="red" />
+                </View>
+            </View>
+        </View>
+    </Modal>
+);
 
 const styles = StyleSheet.create({
     centeredView: {
