9de8642024532320d033c559a9586daa819a6334
Pablo <131611216+pdelalamo@users.noreply.github.com>
2025-01-31 00:10:32 +0100

    feature -> code formatting

--- screens/initialQuestions/Allergies.tsx
+++ screens/initialQuestions/Allergies.tsx
@@ -1,5 +1,5 @@
-import React, { useContext, useState } from 'react';
-import { View, Text, TouchableOpacity, Button, ImageBackground } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, ImageBackground } from 'react-native';
 import { globalStyles } from '../../globalStyles';
 import { initialQuestionsStyles } from './initialQuestionsStyles';
 import { I18nextProvider } from 'react-i18next';
@@ -7,45 +7,49 @@ import i18n from '../../i18n';
 import { t } from 'i18next';
 import { useUserPreferences } from '../../context/UserPreferencesContext';
 
-const allergyOptions: string[] = t('allergyOptions', { returnObjects: true });
-
 interface Props {
     navigation: any;
 }
 
 const Allergies: React.FC<Props> = ({ navigation }) => {
-    // Usage of the context to access the user preferences and update the allergies list
     const { addAllergy, removeAllergy } = useUserPreferences();
-
-    // Initialize selectedAllergies to an empty array
     const [selectedAllergies, setSelectedAllergies] = useState<string[]>([]);
+    const allergyOptions: string[] = t('allergyOptions', { returnObjects: true });
 
-    const handleSelectAllergy = (allergy: string) => {
-        if (selectedAllergies.includes(allergy)) {
-            setSelectedAllergies(prev => prev.filter(a => a !== allergy));
-            removeAllergy(allergy);
-        } else {
-            setSelectedAllergies(prev => [...prev, allergy]);
-            addAllergy(allergy);
-        }
+    const toggleAllergySelection = (allergy: string) => {
+        setSelectedAllergies(prevSelectedAllergies => 
+            prevSelectedAllergies.includes(allergy) 
+                ? prevSelectedAllergies.filter(a => a !== allergy)
+                : [...prevSelectedAllergies, allergy]
+        );
+        selectedAllergies.includes(allergy) ? removeAllergy(allergy) : addAllergy(allergy);
     };
+
+    const renderAllergyButton = (allergy: string) => (
+        <TouchableOpacity 
+            key={allergy}
+            style={selectedAllergies.includes(allergy) ? initialQuestionsStyles.selectedButton : initialQuestionsStyles.button}
+            onPress={() => toggleAllergySelection(allergy)}
+        >
+            <Text style={globalStyles.buttonText}>{allergy}</Text>
+        </TouchableOpacity>
+    );
 
     return (
         <I18nextProvider i18n={i18n}>
-            <ImageBackground source={require('../../assets/images/main_background.png')} resizeMode="cover" style={globalStyles.imageBackground}>
+            <ImageBackground 
+                source={require('../../assets/images/main_background.png')} 
+                resizeMode="cover" 
+                style={globalStyles.imageBackground}
+            >
                 <View style={initialQuestionsStyles.container}>
                     <Text style={initialQuestionsStyles.title}>{t('anyAllergies')}</Text>
-                    {allergyOptions.map((allergy) => (
-                        <TouchableOpacity
-                            key={allergy}
-                            style={selectedAllergies.includes(allergy) ? initialQuestionsStyles.selectedButton : initialQuestionsStyles.button}
-                            onPress={() => handleSelectAllergy(allergy)}
-                        >
-                            <Text style={globalStyles.buttonText}>{allergy}</Text>
-                        </TouchableOpacity>
-                    ))}
+                    {allergyOptions.map(renderAllergyButton)}
                     <View style={globalStyles.buttonContainerFeatures}>
-                        <TouchableOpacity style={globalStyles.buttonGreen} onPress={() => navigation.navigate('MeasurementPreferences')}>
+                        <TouchableOpacity 
+                            style={globalStyles.buttonGreen} 
+                            onPress={() => navigation.navigate('MeasurementPreferences')}
+                        >
                             <Text style={globalStyles.buttonText}>{t('continue')}</Text>
                         </TouchableOpacity>
                     </View>
